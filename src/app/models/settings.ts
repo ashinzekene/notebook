30dcaada0d6e05105831ae8f0c3196ac9ad54b27
Ekene Ashinze <ashinzekene@gmail.com>
2019-08-12 08:07:44 +0100

    enabled purple theme

--- src/app/models/settings.ts
+++ src/app/models/settings.ts
@@ -2,7 +2,7 @@ export interface Settings {
   theme: AppTheme;
 }
 
-export type AppTheme = 'dark'|'pink'| 'blue'| 'black'| 'red'| 'green';
+export type AppTheme = 'dark'|'pink'| 'blue'| 'black'| 'red'| 'green'|'purple';
 
 type ThemeObject = { [K in AppTheme]: string };
 
@@ -10,6 +10,7 @@ export  const themes: ThemeObject = {
   dark: 'Low light',
   pink: 'Pink',
   blue: 'Blue',
+  purple: 'Purple',
   black: 'Black',
   red: 'Red',
   green: 'Green',
